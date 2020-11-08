88cb06033eee9bbfad2bc970b9c75012b6407ead
LuanLuu <lluu3@mail.sfsu.edu>
2020-11-07 22:24:00 -0800

    Initial Commit

---
+++ SkySonic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkySonic
+//
+//  Created by Luan Luu on 11/7/20.
+//  Copyright © 2020 Luan Luu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
