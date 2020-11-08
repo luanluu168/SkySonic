e676529989c1f8bc5d99bc4eb1a34b4c08edb69f
LuanLuu <lluu3@mail.sfsu.edu>
2020-11-07 23:11:10 -0800

    Model Track added

---
+++ SkySonic/Model/itunes.swift
@@ -0,0 +1,27 @@
+//
+//  itunes.swift
+//  SkySonic
+//
+//  Created by Luan Luu on 11/7/20.
+//  Copyright © 2020 Luan Luu. All rights reserved.
+//
+
+import UIKit
+
+struct Track: Codable {
+    let artistName: String
+    let trackName: String
+    let artworkUrl: String
+    let previewUrl: String
+    let collectionName: String?
+    let price: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case artistName = "artistName"
+        case trackName = "trackName"
+        case artworkUrl = "artworkUrl100"
+        case previewUrl = "previewUrl"
+        case collectionName = "collectionName"
+        case price = "trackPrice"
+    }
+}
