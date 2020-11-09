07a553ab8fd69458cff16c68073b04929631cb6f
LuanLuu <lluu3@mail.sfsu.edu>
2020-11-09 09:38:00 -0800

    HomeView Controller added

---
+++ SkySonic/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  SkySonic
+//
+//  Created by Luan Luu on 11/9/20.
+//  Copyright © 2020 Luan Luu. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
