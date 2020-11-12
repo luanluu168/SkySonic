28609a7570ecb57f6bc591db286ffd82437aa978
LuanLuu <lluu3@mail.sfsu.edu>
2020-11-11 16:07:59 -0800

    Modify tabBar icon and background color

--- SkySonic/AppDelegate.swift
+++ SkySonic/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor(red: 0.32, green: 0.105, blue: 0.576, alpha: 1)
+        UITabBar.appearance().backgroundColor = .gray
         return true
     }
 
