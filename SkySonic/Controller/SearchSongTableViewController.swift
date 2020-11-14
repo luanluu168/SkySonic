52165bc8c221f5371188ebf0042202f3f05aeaf8
LuanLuu <lluu3@mail.sfsu.edu>
2020-11-13 21:30:28 -0800

    Clean up code in SearchSongTableView

--- SkySonic/Controller/SearchSongTableViewController.swift
+++ SkySonic/Controller/SearchSongTableViewController.swift
@@ -74,8 +74,8 @@ class SearchSongTableViewController: UITableViewController, UISearchBarDelegate 
                 
                 if let data = data,
                     let image = UIImage(data: data) {
+                    // use main thread to set the image for each row
                     DispatchQueue.main.async {
-                        // use main thread to set the image for each row
                         cell.imageView?.image = image.resizeImageToSmaller(80.0)
                         cell.imageView?.makeRounded(4.0)
                     }
@@ -162,61 +162,5 @@ class SearchSongTableViewController: UITableViewController, UISearchBarDelegate 
         }
         searchDataTask.resume()
     }
-    
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
